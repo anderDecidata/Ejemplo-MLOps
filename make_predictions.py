b9d699bc3bc2aa5fc338116666a4636bbceb0b50
Ander Fernandez <afernandez@decidata.es>
2021-12-01 12:27:51 +0100

    Fix make_predictions.py

--- make_predictions.py
+++ make_predictions.py
@@ -1,4 +1,4 @@
 import requests
-url = 'https://ejemplo-mlops-qc5dfzkvdq-ew.a.run.app'
+url = 'https://ejemplo-mlops-qc5dfzkvdq-ew.a.run.app/forecast'
 resp = requests.post(url, params={'return_predicciones': 'True'})
 print(resp.content)
